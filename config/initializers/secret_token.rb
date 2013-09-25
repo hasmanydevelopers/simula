a7724501bb71540531c1f0b981a669e41df155ee
LuciaMariana <lucia.m.lo.monaco@gmail.com>
2013-09-25 08:39:19 -0430

    Inicializa repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PsicoApp::Application.config.secret_key_base = '6df6b9e50995c1d0c347b4869c950fa36a81bc6b072dfb262b7545c4cdea7846a616f929c7459718a2ee55cef8887add88f294ae11729f4383555d7d79aebe22'
