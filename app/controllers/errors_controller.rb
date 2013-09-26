421a82a3e76de5936e9be4afe082bc7682a62d35
LuciaMariana <lucia.m.lo.monaco@gmail.com>
2013-09-26 10:46:27 -0430

    Impedir el singup

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,5 +1,5 @@
 class ErrorsController < ApplicationController
-  skip_before_filter
+  skip_before_filter :authenticate_user!
   def routing
    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
   end
