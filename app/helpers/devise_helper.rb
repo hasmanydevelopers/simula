462c31bebded289fb7ca8246dcba3afa68b8eb71
LuciaMariana <lucia.m.lo.monaco@gmail.com>
2013-10-08 10:19:36 -0430

    Arreglar estilaje de errores para que luzcan igual que las alertas de devise

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,18 @@
+module DeviseHelper
+
+  def devise_error_messages!
+    return "" if resource.errors.empty?
+
+    message =  content_tag(:p, resource.errors.full_messages.first, :class => "alert alert-danger")
+
+    html = <<-HTML
+    <div id="error_explanation">
+      #{message}
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+end
+
