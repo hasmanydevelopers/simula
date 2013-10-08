b21273a24fcf9259b891a1082e9a31e5f4fae5c7
LuciaMariana <lucia.m.lo.monaco@gmail.com>
2013-10-08 10:42:20 -0430

    Arreglar error que sucede cuando resource no existe

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,7 +1,7 @@
 module DeviseHelper
 
   def devise_error_messages!
-    return "" if resource.errors.empty?
+    return "" if resource.nil? || resource.errors.empty?
 
     message =  content_tag(:p, resource.errors.full_messages.first, :class => "alert alert-danger")
 
