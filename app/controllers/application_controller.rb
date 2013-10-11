b4faba99530f652431cfc9da6b23c3df38acdfec
LuciaMariana <lucia.m.lo.monaco@gmail.com>
2013-10-11 16:37:33 -0430

    Añadir los campos first_name y last_name a la forma de 'Preferences' y agregar en nombre del usuario en el botón colapsable y en el menú desplegado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
 
+  before_filter :configure_devise_params, if: :devise_controller?
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    end
+  end
+
 end
