ce98c6658a45a2f4c748a84d361d9c37a1063a32
LuciaMariana <lucia.m.lo.monaco@gmail.com>
2013-10-16 15:14:03 -0430

    Hacer la tabla dinámica de manera que aparezca el nombre y apellido de cada usuario que me ha supervisado y el número de veces que lo ha hecho

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,13 @@ class HomeController < ApplicationController
     def index
         @sessions_as_therapist = current_user.sessions_as_therapist.count
         @sessions_as_patient = current_user.sessions_as_patient.count
+        total_sessions = current_user.therapy_sessions
+        supervisor_vs_times = Hash.new(0)
+        total_sessions.each do |session|
+            supervisor = Users::Supervisor.find(session.supervisor_id)
+            supervisor_complete_name = "#{supervisor.first_name} #{supervisor.last_name}"
+            supervisor_vs_times[supervisor_complete_name] = supervisor_vs_times[supervisor_complete_name] + 1
+        end
+        @supervisor_vs_times = supervisor_vs_times
     end
 end
--- app/models/users/student.rb
+++ app/models/users/student.rb
@@ -1,4 +1,8 @@
 class Users::Student < User
   has_many :sessions_as_therapist, foreign_key: "therapist_id", class_name: "TherapySession"
   has_many :sessions_as_patient, foreign_key: "patient_id", class_name: "TherapySession"
+
+  def therapy_sessions
+      return self.sessions_as_therapist + self.sessions_as_patient
+  end
 end
