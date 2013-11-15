c89d6fc98a0e95ad37962792c6d9d886d815a78c
LuciaMariana <lucia.m.lo.monaco@gmail.com>
2013-11-15 09:45:06 -0430

    Agregar path de fonts, arreglar urls de fonts en el archivo de estilaje de bootstrap y quitar recursos innecesarios en el layout de la app.

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ Bundler.require(:default, Rails.env)
 
 module PsicoApp
   class Application < Rails::Application
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
     config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
