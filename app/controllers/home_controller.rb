e85a3bce87b03d900c0c802530c74ac9136aeab0
LuciaMariana <lucia.m.lo.monaco@gmail.com>
2013-11-18 10:31:21 -0430

    Arreglar la posición de los mensajes de sesiones agregadas por otros compañeros, de manera que sólo aparezcan en el home del estudiante

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
     def index
-        messages = new_sessions_advise
-        flash.now[:info] = messages
         if current_user.type == "Users::Student"
             @sessions_as_therapist_confirmed = current_user.sessions_as_therapist.where(state: :confirmed).count
             @sessions_as_patient_confirmed = current_user.sessions_as_patient.where(state: :confirmed).count
@@ -10,6 +8,8 @@ class HomeController < ApplicationController
             @sessions_as_therapist_rejected = current_user.sessions_as_therapist.where(state: :rejected).count
             @sessions_as_patient_rejected = current_user.sessions_as_patient.where(state: :rejected).count
             @supervisor_vs_times = supervisor_vs_times
+            messages = new_sessions_advise
+            flash.now[:info] = messages
             render "student_home"
         else
             redirect_to therapy_sessions_path
